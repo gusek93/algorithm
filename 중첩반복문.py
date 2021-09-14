41245ce16d917de5114a664c77519244543b6dad
Dayong Hyun <gusek93@naver.com>
2021-09-14 14:24:22 +0900

    중첩 반복문 문제 풀이

--- 중첩반복문.py
+++ 중첩반복문.py
@@ -1,3 +1,5 @@
+# 코드업 충첩 반복문 기초 문제 풀이
+
 # 1351
 # first = int(5)
 # last = int(9)
@@ -57,3 +59,128 @@
 #             print(' ', end='')
 #         print('*', end='')
 #         print()
+
+
+# 1357
+# n = int(7)
+
+# for i in range(1, n+1):
+#     print('*' * i)
+
+# for j in range(n-1, 0, -1):
+#     print('*' * j)
+
+
+# 1358
+# n = int(5)
+
+# for i in range(1, n + 1):
+#     if i % 2 != 0:
+#         a = '*'*i
+#         print(a.center(n))
+
+
+# 1361
+# n = int(3)
+
+# for i in range(0, n):
+#     print(' ' * i, end='')
+#     print('**', end='')
+#     print()
+
+
+# 1365
+# 잘 이해 못하겠다.. if 조건을 자세히 보자
+# n = int(9)
+
+# for i in range(0, n):
+#     print('*', end='')
+# print()
+
+# for i in range(1, n-1):
+#     for j in range(0, n):
+#         if((j == 0) or (j == i) or (j == n-1) or (j == n-i-1)):
+#             print('*', end='')
+#         else:
+#             print(' ', end='')
+
+#     print()
+
+# for i in range(0, n):
+#     print('*', end='')
+# print()
+
+
+# 1366
+
+# n = int(17)
+# for i in range(0, n):
+#     print('*', end='')
+# print()
+
+# for i in range(1, n-1):
+#     for j in range(0, n):
+#         if((j == 0) or (j == i) or (j == n-1) or (j == n-i-1) or (n-1)/2 == j or (n-1)/2 == i):
+#             print('*', end='')
+#         else:
+#             print(' ', end='')
+
+#     print()
+
+# for i in range(0, n):
+#     print('*', end='')
+# print()
+
+
+# 1367
+# n = int(4)
+
+# for i in range(n-1, 0, -1):
+#     print(' ' * i, end='')
+#     print('*' * n, end='')
+#     print()
+
+# print('*'*n)
+
+
+# 1368
+# h = int(5)
+# k = int(4)
+# d = 'R'
+
+# if d == 'L':
+#     for i in range(0, h):
+#         print(' '*i, end='')
+#         print('*' * k, end='')
+#         print()
+
+# else:
+#     for i in range(h-1, 0, -1):
+#         print(' ' * i, end='')
+#         print('*' * k, end='')
+#         print()
+#     print('*'*k)
+
+# 중첩반복으로 풀이
+# h = int(5)
+# k = int(4)
+# d = 'R'
+
+# if d == 'L':
+#     for i in range(0, h):
+#         for j in range(0, i):
+#             print(' ', end='')
+#         for j in range(0, k):
+#             print('*', end='')
+#         print()
+
+# else:
+#     for i in range(h-1, -1, -1):
+#         for j in range(0, i):
+#             print(' ', end='')
+#         for j in range(0, k):
+#             print('*', end='')
+#         print()
+
+
+# 1369
