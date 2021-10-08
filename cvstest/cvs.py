e240b6f6a8817ca75626dacc0137d81c4511bfb7
Dayong Hyun <gusek93@naver.com>
2021-10-08 23:45:33 +0900

    월별 데이터 추출하여 join

--- cvstest/cvs.py
+++ cvstest/cvs.py
@@ -1,73 +1,45 @@
 import pandas as pd
 import warnings
 
+# 엑셀 읽어오기
 with warnings.catch_warnings(record=True):
     warnings.simplefilter("always")
     df = pd.read_excel(
-        "/Users/dayong/project/algorithm/cvstest/cvsdata/부산환경_2020_사원등록_20211001_111155_.xlsx", engine="openpyxl", header=int(9), usecols="B,C,D")
+        "/Users/dayong/project/algorithm/cvstest/cvsdata//사원등록_20211001_111155_.xlsx", engine="openpyxl", header=int(9), usecols="B,C,D")
 
+# NaN 데이터 제외한 데이터 출력
 data = df.dropna(how='all')
 
+list_from_df = data.values.tolist()
 
-test = pd.DataFrame(data)
-list_from_df = test.values.tolist()
-# print(list_from_df)
-# print(test.shape[0])
-# print(test.shape)
-# for i in list_from_df:
-#     result = i.columns
-# print(test)
-# print(list_from_df[3][0])
+# 리스트 선언
 no_list = []
 number_list = []
 name_list = []
 iny_list = []
 tny_list = []
 
-for i in range(1, test.shape[0]):
+# 데이터 정제
+for i in range(1, data.shape[0]):
     if i % 2 != 0:
         no_list.append(int(list_from_df[i][0]))
-        # print(list_from_df[i][0])
-
-for i in range(1, test.shape[0]):
-    if i % 2 != 0:
-        # print(list_from_df[i][1])
         number_list.append(list_from_df[i][1])
-for i in range(1, test.shape[0]):
-    if i % 2 != 0:
-        # print(list_from_df[i][2])
         name_list.append(list_from_df[i][2])
 
-for i in range(1, test.shape[0]):
-    if i % 2 == 0:
+    elif i % 2 == 0:
         iny_list.append(list_from_df[i][1])
         tny_list.append(list_from_df[i][2])
 
-# print(iny_list)
 
-test1 = pd.DataFrame(no_list)
-test2 = pd.DataFrame(number_list)
-test3 = pd.DataFrame(name_list)
-test4 = pd.DataFrame(iny_list)
-test5 = pd.DataFrame(tny_list)
-result = pd.concat([test1, test2, test3, test4, test5], axis=1)
+# 데이터 join
+no = pd.DataFrame(no_list)
+number = pd.DataFrame(number_list)
+name = pd.DataFrame(name_list)
+iny = pd.DataFrame(iny_list)
+ony = pd.DataFrame(tny_list)
+
+result = pd.concat([no, number, name, iny, ony], axis=1)
 result.columns = ["NO", "사번", "성명", "입사년월일", "퇴사년월일"]
 
-result.to_excel('succes.xlsx')
-
 print(result)
-
-# print(result)
-# test.to_excel('test.xlsx')
-
-# list_from_df = df.values.tolist()
-# result = []
-# result2 = []
-
-# for i in list_from_df:
-#     if i != 'nan':
-#         result += [i]
-
-# test = pd.DataFrame(result)
-# data = test.dropna(how='all')
-# print(data)
+# result.to_excel('succes.xlsx')
